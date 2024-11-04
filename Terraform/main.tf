0fa3f5d3897504cecad92d09928abf28f5610707
Joe Doran <joe.doran@nettaskforce.com>
2024-11-04 21:42:27 +0000

    Added main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-00385a401487aefa4" # eu-west-1
+  instance_type = "t2.micro"
+  tags = {
+    Name = "NEC Demo instance"
+  }
+}
