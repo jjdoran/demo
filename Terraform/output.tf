dcedb4303d6172b2c4d21269fd0cb45876e41aa3
jjdoran <73179949+jjdoran@users.noreply.github.com>
2024-11-05 21:02:27 +0000

    Update output.tf
    demo-dev-box added to variables

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,13 +1,13 @@
 output "public_dns" { 
 	description	= "The public DNS name associated with the instance"
-	value		= try(aws_instance.demobox.public_dns, "")
+	value		= try(aws_instance.demo-dev-box.public_dns, "")
 }
 output "public_ip" { 
 	description 	= "The public IP Address associated with the instance"
-	value		= try(aws_instance.demobox.public_ip, "")
+	value		= try(aws_instance.demo-dev-box.public_ip, "")
 }
 
 output "private_ip" { 
 	description 	= "The private IP Address associated with the instance"
-	value		= try(aws_instance.demobox.private_ip, "")
+	value		= try(aws_instance.demo-dev-box.private_ip, "")
 }
