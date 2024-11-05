1d2bcd2ba4c503970f32ebd0ef8cf2341a8a29c7
Joe Doran <joe.doran@nettaskforce.com>
2024-11-05 20:44:49 +0000

    .

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,6 +13,7 @@ terraform {
 resource "aws_instance" "demobox" {
 	ami           = "ami-00385a401487aefa4" # eu-west-1
 	instance_type = "t2.micro"
+	key_name = "aws-key"
 	tags = {
 		Name = "NEC Demo instance"
 	}
