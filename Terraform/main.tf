0e818bcf814cd45bb3eb98167440ec2906faf90f
Joe Doran <joe.doran@nettaskforce.com>
2024-11-05 16:46:07 +0000

    .

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 resource "aws_instance" "foo" {
   ami           = "ami-00385a401487aefa4" # eu-west-1
   instance_type = "t2.micro"
   tags = {
     Name = "NEC Demo instance"
-}
+  }
 }
