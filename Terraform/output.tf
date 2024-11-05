583ba64b18ba1549870e7ddc4d6b31c5d79ebb8d
Joe Doran <joe.doran@nettaskforce.com>
2024-11-05 17:55:21 +0000

    .

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,13 +1,13 @@
 output "public_dns" { 
-	description "The public DNS name associated with the instance"
-	value	= try(aws_instance.demobox.public_dns, "")
+	description	= "The public DNS name associated with the instance"
+	value		= try(aws_instance.demobox.public_dns, "")
 }
 output "public_ip" { 
-	description "The public IP Address associated with the instance"
-	value	= try(aws_instance.demobox.public_ip, "")
+	description 	= "The public IP Address associated with the instance"
+	value		= try(aws_instance.demobox.public_ip, "")
 }
 
 output "private_ip" { 
-	description "The private IP Address associated with the instance"
-	value	= try(aws_instance.demobox.private_ip, "")
+	description 	= "The private IP Address associated with the instance"
+	value		= try(aws_instance.demobox.private_ip, "")
 }
