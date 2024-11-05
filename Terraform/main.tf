05ce3c57f8bac3306cd57e9f72e75f4f1bda4bea
Joe Doran <joe.doran@nettaskforce.com>
2024-11-05 15:22:23 +0000

    .

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "${AWS_ACCESS_KEY_ID}"
-  secret_key = "${AWS_SECRET_ACCESS_KEY}"
-  region = "eu-west-1"
-}
-
 resource "aws_instance" "foo" {
   ami           = "ami-00385a401487aefa4" # eu-west-1
   instance_type = "t2.micro"
